0e66832892a3a8758667c43bee0bdd7b35c854fb
phanopera <70485089+phanopera@users.noreply.github.com>
2022-12-25 01:16:27 +0300

    Update quickSort.cpp

--- quickSort/quickSort.cpp
+++ quickSort/quickSort.cpp
@@ -68,60 +68,63 @@ void insertionSort(T* first, T* last, Compare comp) {
 //быстрая сортировка https://www.techiedelight.com/ru/quicksort/
 template<typename T, typename Compare>
 void quickSort(T* first, T* last, Compare comp) {
-    if (first == last) { return; }
-    T* il =first;//0
-    T* ir =last;//z
-    T* mid = first+(last-first)/2;//Центральный элемент массива
-    if (comp(*first, *last)) { //first<last
-        if (comp(*first, *mid)) { //first<mid
-            if (comp(*last, *mid)) {
-                mid = last;//средний - ласт
+        if (first == last) { return; }
+        T* il = first;//0
+        T* ir = last;//z
+        T* mid = first + (last - first) / 2;//Центральный элемент массива
+        if (comp(*first, *last)) { //first<last
+            if (comp(*first, *mid)) { //first<mid
+                if (comp(*last, *mid)) {
+                    mid = last;//средний - ласт
+                }
+                //средний - мид
             }
             //средний - мид
         }
-        //средний - мид
-    }
-    else {//last<first
-        if (comp(*first, *mid)) { //first<mid
-            mid = first;//средний - ферст
-        }
-        else {
-            if (comp(*mid, *last)) {
-                mid = last;//средний - ласт
+        else {//last<first
+            if (comp(*first, *mid)) { //first<mid
+                mid = first;//средний - ферст
             }
-            //средний - ласт
+            else {
+                if (comp(*mid, *last)) {
+                    mid = last;//средний - ласт
+                }
+                //средний - ласт
+            }
         }
-    }
-    do {
-        //Пробегаем элементы, ищем те, которые нужно перекинуть в другую часть
-        //В левой части массива пропускаем(оставляем на месте) элементы, которые меньше центрального
-        while (*il<*mid) {
-            il++;
-        }
-        while (*ir >*mid ) {
-            ir--;
-        }
-        //Меняем элементы местами
-        if (il <= ir) {
-            if (*il > *ir) { std::swap(*il, *ir); }
-            
-            il++;
-            ir--;
-        }
-    } while (il <= ir);
-    // выбор крайне правого элемент в качестве опорного элемента массива
 
-    //Рекурсивные вызовы, если осталось, что сортировать
-    if (ir > first) {
-        //"Левый кусок"
-        quickSort(first, ir+1, comp);
-    }
-    if (il < last) {
-        //"Прaвый кусок"
-        quickSort(first,last - 1, comp);
-    }
+
+        const T value = *mid;
+
+        do {
+            //Пробегаем элементы, ищем те, которые нужно перекинуть в другую часть
+            //В левой части массива пропускаем(оставляем на месте) элементы, которые меньше центрального
+            while (*il < value) {
+                il++;
+            }
+            while (*ir > value) {
+                ir--;
+            }
+            //Меняем элементы местами
+            if (il <= ir) {
+                if (*il > *ir) { std::swap(*il, *ir); }
+
+                il++;
+                ir--;
+            }
+        } while (il <= ir);
+        // выбор крайне правого элемент в качестве опорного элемента массива
+
+        //Рекурсивные вызовы, если осталось, что сортировать
+        if (ir > first) {
+            //"Левый кусок"
+            quickSort(first, ir, comp);
+        }
+        if (il < last) {
+            //"Прaвый кусок"
+            quickSort(il, last, comp);
+        }
 }
-
 
 
 void addDigits(int* mas, int size) {

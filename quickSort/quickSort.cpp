1e00575da0ecd0e569cfa4d9417c83d194372520
phanopera <70485089+phanopera@users.noreply.github.com>
2022-12-25 05:10:35 +0300

    Update quickSort.cpp

--- quickSort/quickSort.cpp
+++ quickSort/quickSort.cpp
@@ -1,5 +1,68 @@
 #include "qs.h"
 
+void getBenchTime() {
+    LARGE_INTEGER frequency{ 0 }; // ticks per second
+    LARGE_INTEGER init_ticks{ 0 };
+    LARGE_INTEGER curr_ticks{ 0 };
+    std::ofstream fout;
+    fout.open("TimeOutput.txt", std::ios_base::trunc);
+    if (!fout.is_open())
+    {
+        std::cout << "Ошибка при открытии файла!" << std::endl;
+    }
+    else
+    {
+        QueryPerformanceFrequency(&frequency);
+        for (int size = 2; size <= 50; size++)
+        {
+            int* mas = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = i;
+
+            }
+            QueryPerformanceCounter(&init_ticks);
+            //соник-сортировка
+            for (int j = 0; j < 10000; j++)
+            {
+                quickSort(mas, mas+ size-1, [](int a, int b) { return a > b; });
+                quickSort(mas, mas+ size-1, [](int a, int b) { return a < b; });
+            }
+            QueryPerformanceCounter(&curr_ticks);
+            fout << "quickSort, size: " << size << "; time: "
+                << (double)((curr_ticks.QuadPart - init_ticks.QuadPart) * 1000000 / frequency.QuadPart) << std::endl;
+
+           
+            QueryPerformanceCounter(&init_ticks);
+            //вставочки
+            for (int j = 0; j < 10000; j++)
+            {
+                insertionSort(mas, mas + size-1, [](int a, int b) { return a > b; });
+                insertionSort(mas, mas + size-1, [](int a, int b) { return a < b; });
+            }
+            QueryPerformanceCounter(&curr_ticks);
+            fout << "insertSort, size: " << size << "; time: "
+                << (double)((curr_ticks.QuadPart - init_ticks.QuadPart) * 1000000 / frequency.QuadPart) << std::endl;
+
+            
+            QueryPerformanceCounter(&init_ticks);
+            //супер мега ультра нано сортировка
+            for (int j = 0; j < 10000; j++)
+            {
+                quickSort(mas, mas + size - 1, [](int a, int b) { return a > b; });
+                quickSort(mas, mas + size - 1, [](int a, int b) { return a < b; });
+            }
+            QueryPerformanceCounter(&curr_ticks);
+            fout << "sort, size: " << size << "; time: "
+                << (double)((curr_ticks.QuadPart - init_ticks.QuadPart) * 1000000 / frequency.QuadPart) << std::endl;
+
+            delete[] mas;
+        }
+        fout.close();
+    } 
+
+}
+
 int main()
 {
     int size = 20;
@@ -14,4 +77,6 @@ int main()
     std::cout << "\n";
     sort(a, a + size - 1, [](int a, int b) { return a < b; });
     showDigits(a, size);
+    
+    getBenchTime() ;
 }
